fc9b0d2eca28e539daebfa286801e5d6034109e1
Matheus <Matheus@192.168.0.27>
2018-10-13 21:56:03 -0300

    alteracao servidor

--- ep1_redes_servidor/src/ep1_redes_servidor/Server.java
+++ ep1_redes_servidor/src/ep1_redes_servidor/Server.java
@@ -8,34 +8,44 @@ class Server
    public static void main(String argv[]) throws Exception
       {
  
-   		 System.out.println(" Server is Running  " );
+            System.out.println(" Server is Running  " );
          ServerSocket mysocket = new ServerSocket(5555);
- 
+         //comentario
          while(true)
          {
             Socket connectionSocket = mysocket.accept();
  
             BufferedReader reader =
-            		new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             BufferedWriter writer= 
-            		new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
+                    new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
  
-            writer.write("*** Welcome to the Calculation Server (Addition Only) ***\r\n");            
-            writer.write("*** Please type in the first number and press Enter : \n");
+            writer.write(" Bem-vindo ao incrivel HUB de games do EP de Redes!!!! \r\n");
+            writer.write("*** Insira o numero correspondente ao jogo que deseja jogar e pressione Enter : \n"
+                    + "1 - Forca\n"
+                    + "2 - Velha\n"
+                    + "3 - Pedra-Papel-Tesoura\n");
             writer.flush();
             String data1 = reader.readLine().trim();
  
-            writer.write("*** Please type in the second number and press Enter : \n");
-            writer.flush();
-            String data2 = reader.readLine().trim();
+            int opcaoJogo=Integer.parseInt(data1);
+            String respostaOpcaoJogo = "Numero invalido!";
  
-            int num1=Integer.parseInt(data1);
-            int num2=Integer.parseInt(data2);
+            switch(opcaoJogo){
+            case 1:
+                respostaOpcaoJogo = "Jogo da Forca selecionado!";
+                break;
+            case 2:
+                respostaOpcaoJogo = "Jogo da Velha selecionado!";
+                break;
+            case 3:
+                respostaOpcaoJogo = "Pedra-Papel-Tesoura selecionado!";
+                break;
+            }
+
+            System.out.println("Opcao de jogo selecionada.");
  
-            int result=num1+num2;            
-            System.out.println("Addition operation done " );
- 
-            writer.write("\r\n=== Result is  : "+result);
+            writer.write("\r\n"+respostaOpcaoJogo);
             writer.flush();
             connectionSocket.close();
          }
